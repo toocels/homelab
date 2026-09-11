1c7d41c8c35e48b5b0e0d6acf5da5b4ff6275074
toocels <toocels@gmail.com>
2026-09-11 08:55:42 +0530

    added dockge, 🤌

--- start-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-SERVICES=(beszel jellyfin navidrome nextcloud nginx pihole portainer tailscale vaultwarden)
-for s in "${SERVICES[@]}"; do
-  docker compose -f "./$s/docker-compose.yml" up -d
-done
